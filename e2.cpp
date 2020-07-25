1bad0843cb281a088fdb4c7c9c7ab6514033e05d
Coder-Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2020-07-25 10:40:52 +0530

    initial

---
+++ e2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+int i,n;
+cout<<"enter the number:"<<endl;
+cin>>n;
+if(n%2!=0)
+{
+    cout<<"number is prime"<<endl;
+}
+else
+{
+    cout<<"number is  not prime"<<endl;
+}
+}
