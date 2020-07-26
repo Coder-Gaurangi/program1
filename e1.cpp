708a057f615cf0011091db91ba1b57f61de359b5
Coder-Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2020-07-26 16:44:50 +0530

    initial

---
+++ e1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,s=0,t,num,n;
+    cout<<"enter the number"<<endl;
+    cin>>num;
+    n=num;
+    for(i=0;i<=num;i++)
+    {
+        t=num%10;
+        s=(s*10)+t;
+        num=num/10;
+    }
+    cout<<"the reverse number is-"<<s<<endl;
+    if(n==s)
+    {
+        cout<<"the given number is palindrome"<<endl;
+    }
+    else
+    {
+        cout<<"the given number is palindrome"<<endl;
+    }
+}
